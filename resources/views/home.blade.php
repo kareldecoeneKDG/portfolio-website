075ad57a90e8ac64b99667c0cc6df8d491065878
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-06-14 07:39:39 +0200

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
 
         <title>KAREL - NxT Web Developer</title>
 
