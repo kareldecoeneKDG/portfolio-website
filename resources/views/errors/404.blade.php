2b30c60599adcf15e3e6286f90bfb4958370d099
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-06-25 15:14:06 +0200

    test

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -18,7 +18,10 @@
         <!-- Facebook Preview Tags -->
         <meta property="og:title" content="Karel Decoene - NxT Media Technology - Portfolio Website">
         <meta name="og:description" content="Hi, I'm Karel and I'm a student NxT Media Technology at KdG Hogeschool learling Web Development and UX/UI. Welcome to my portfolio website.">
-        <meta property="og:image" content="{{ URL::asset('../../public/img/facebook.png') }}">
+        <meta name="image" property="og:image" content="{{ URL::asset('../../public/img/facebook.png') }}">
+
+        <!-- LinkedIn Preview Tag -->
+        <meta name="author" content="Karel Decoene">
 
         <title>Error | KAREL - NxT Web Developer</title>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,6 +21,8 @@
         <meta property="og:title" content="Karel Decoene - NxT Media Technology - Portfolio Website">
         <meta name="og:description" content="Hi, I'm Karel and I'm a student NxT Media Technology at KdG Hogeschool learling Web Development and UX/UI. Welcome to my portfolio website.">
         <meta name="image" property="og:image" content="{{ URL::asset('../../public/img/facebook.png') }}">
+
+        <!-- LinkedIn Preview Tag -->
         <meta name="author" content="Karel Decoene">
 
         <title>KAREL - NxT Web Developer</title>
